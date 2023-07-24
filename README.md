fae3116e32b37556cdbfa65e3e9489a6a5528b60
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-07-24 16:34:29 +0500

    feat: make a simple signup page
    this page was not made by using atomic commits because the header and the footer are copy-pasted from main page, and the signup form was made by following the tutorial linked in the README file

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ This is the second project of the TOP(The Odin Project) course(https://www.theod
 Image credits for solution photos : The Odin Project www.theodinproject.com
 
 Image overlay feature credits: w3schools https://www.w3schools.com/css/css3_images.asp
+
+Signup page credits: The Net Ninja 
+
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9g6m_6Sld9Q4jzqdqHd2HiD
+
+
+https://github.com/iamshaunjp/regex-playlist
+
---
+++ signup.js
@@ -0,0 +1,32 @@
+const inputs = document.querySelectorAll('input');
+
+
+
+const regex = {
+
+  username: /^[a-z0-9]{5,12}$/i,
+  email: /^([a-z\d.-]+)@([a-z]+)\.([a-z]+)?$/,
+  password: /^[\w@-]{8,20}$/,
+  telephone: /^[\d]{11}$/
+
+}
+
+
+
+function validate(field, regex) {
+
+  field.className = (regex.test(field.value))?"valid":"invalid";
+
+}
+
+
+
+inputs.forEach((input) => {
+  
+  input.addEventListener('keyup', (e) => {
+
+    validate(e.target, regex[e.target.attributes.name.value]);
+
+  });
+
+});
