bec2bf43a81ffe58f4462e30aa895dcca6c7b09d
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-07-25 16:27:20 +0500

    feat: assign class to form-button for form status
    this will enable/disable the css rules for the valid and invalid classes of form-button. the logic used is similiar to the logic of checking individual fields

--- signup.js
+++ signup.js
@@ -26,10 +26,27 @@ inputs.forEach((input) => {
   input.addEventListener('keyup', (e) => {
 
     validate(e.target, regex[e.target.attributes.name.value]);
-
+    
   });
 
 });
 
 const formButton = document.getElementById('form-button');
+let success = true;
 
+formButton.addEventListener('click', (e) => {
+
+  inputs.forEach((input) => {
+    
+    if ((regex[input.attributes.name.value].test(input.value))) {
+      success = true;//this condition was needed for setting success to true if it had been set to false after the last click
+    }
+    else  {
+      success = false;
+    }
+
+  });
+
+  formButton.className=success?"valid":"invalid";
+
+});
